1d9f3630e47fc4ba31f70988b0360895a5aa5133
Tirth Vanparia <144980949+TechTirth@users.noreply.github.com>
2024-07-05 15:10:02 +0530

    mobile left nav bar and little bit of ui for rightside bar for desktop view

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,37 @@
 import React from "react";
 import HeaderBox from "@/components/HeaderBox";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 
 const Home = () => {
-  const loggedIn = {firstName: 'Tirth'}
+  const loggedIn = {
+    firstName: "Tirth",
+    lastName: "Vanparia",
+    email: "t1eerth.v@gmail.com",
+  };
 
-  return <section className="home">
-    <div className="home-content">
-      <header className="home-header">
-        <HeaderBox
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || 'Guest'}
-          subtext="Access and manage your account and transactions efficiently." 
-        />
-        <TotalBalanceBox
-        accounts={[]}
-        totalBanks={1}
-        totalCurrentBalance={1250.35}
-        />
-      </header>
-    </div>
-  </section>;
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        Recent Transactions
+      </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
+    </section>
+  );
 };
 
 export default Home;
